f74d0c3010270c8d39982a46bbd440938567c753
Sebery <seberyincorporation@gmail.com>
2021-02-03 20:28:32 -0600

    Added: Player Smooth Movement and Animations

---
+++ Scripts/GunController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private int currentGun;
+    [SerializeField] private SpriteRenderer[] guns;
+    [SerializeField] private float changeLayerRange;
+    private Vector2 mousePos;
+    private Vector2 dir;
+    private float angle;
+
+    private void Update() {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Rotate();
+        ChangeSortingLayer();
+    }
+
+    private void Rotate() {
+        dir = mousePos - (Vector2)transform.position;
+        angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, (angle + 180.0f) * -1.0f);
+    }
+
+    private void ChangeSortingLayer() {
+        Vector2 animDirP = playerController.GetAnimDirection();
+        int currentLayerP = playerController.GetCurrentSortingLayer();
+
+        if (animDirP.y > 0.0f && animDirP.x <= changeLayerRange && animDirP.x >= -changeLayerRange)
+            guns[currentGun].sortingOrder = currentLayerP - 1;
+        else
+            guns[currentGun].sortingOrder = currentLayerP + 1;
+    }
+
+    //Getters And Setters
+    public Vector2 GetDirection() => dir;
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerController : MonoBehaviour {
+    [SerializeField] private float speed;
+    [SerializeField] private GunController gunController;
+
+    private Vector2 dir;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+    private Vector2 animDir = new Vector2(0.0f, -1.0f);
+
+    private void Start() {
+        Init();
+    }
+
+    private void Update() {
+        CalcMoveDirection();
+        CalcAnimDirection();
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = speed * Time.deltaTime * dir;
+    }
+
+    private void CalcMoveDirection() {
+        dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        dir = (dir.sqrMagnitude > 1.0f) ? dir.normalized : dir;
+        anim.SetBool("isMoving", dir != Vector2.zero);
+    }
+
+    private void Init() {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void CalcAnimDirection() {
+        animDir = gunController.GetDirection().normalized;
+        anim.SetFloat("dirX", animDir.x);
+        anim.SetFloat("dirY", animDir.y);
+    }
+
+    //Getters And Setters
+    public int GetCurrentSortingLayer() => sprite.sortingOrder;
+    public Vector2 GetAnimDirection() => animDir;
+}
