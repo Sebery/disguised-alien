cb2ddd882149597070d22bdfc0680d9dab245565
Sebery <seberyincorporation@gmail.com>
2021-02-15 19:02:32 -0600

    Player Movement with Gun

---
+++ Scripts/GunController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+    [SerializeField] private SpriteRenderer[] guns;
+
+    private Manager manager;
+    private GameController gameController;
+    private PlayerController playerController;
+    private Vector2 direction = new Vector2(1.0f, 0.0f);
+    private float angle;
+
+    private void Start() {
+        manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<Manager>();
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        playerController = GetComponentInParent<PlayerController>();
+    }
+
+    private void Update() {
+        if (!manager.Mobile) {
+            direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+        } else {
+            if (gameController.ShootJoystick.Direction != Vector2.zero)
+                direction = gameController.ShootJoystick.Direction;
+        }
+
+        ChangeSortingLayer();
+        SetAngle();
+    }
+
+    private void SetAngle() {
+        angle = (Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg * -1.0f) - 180.0f;
+        transform.localRotation = Quaternion.Euler(0.0f, 0.0f, angle);
+    }
+
+    private void ChangeSortingLayer() {
+        if (playerController.IsMovingUp)
+            guns[playerController.CurrentGun].sortingOrder = playerController.SortingOrder - 1;
+        else
+            guns[playerController.CurrentGun].sortingOrder = playerController.SortingOrder + 1;
+    }
+
+
+}
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,35 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
     [SerializeField] private float speed;
     [SerializeField] private int lives;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
 
     //Animation Parameters
-    private const string LASTDIRX = "LastDirX";
-    private const string LASTDIRY = "LastDirY";
-    private const string DIRX = "DirX";
-    private const string DIRY = "DirY";
-    private const string ISMOVING = "IsMoving";
+    private const string DIR_X = "DirX";
+    private const string DIR_Y = "DirY";
+    private const string IS_MOVING = "IsMoving";
     private const string DIE = "Die";
 
-    private float lastDirX;
-    private float lastDirY;
     private Manager manager;
     private GameController gameController;
     private Rigidbody2D _rigidbody2D;
     private Animator _animator;
-    private Vector2 direction = Vector2.zero;
+    private Vector2 moveDirection;
+    private Vector2 animDirection;
+    private bool isMovingUp = false;
+    private int currentGun = 0;
+
+    public int SortingOrder { get => _spriteRenderer.sortingOrder; }
+    public int CurrentGun { get => currentGun; }
+    public bool IsMovingUp { get => isMovingUp; }
 
     private void Start() {
-        lastDirX = 1.0f;
-        lastDirY = 0.0f;
         manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<Manager>();
         gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _animator = GetComponentInChildren<Animator>();
+        _animator.SetFloat(DIR_X, 1.0f);
+        _animator.SetFloat(DIR_Y, 0.0f);
+        animDirection.x = 1.0f;
+        animDirection.y = 0.0f;
     }
 
     private void Update() {
@@ -39,36 +46,37 @@ public class PlayerController : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        _rigidbody2D.velocity = speed * Time.deltaTime * direction;
+        _rigidbody2D.velocity = speed * Time.deltaTime * moveDirection;
     }
 
     private void SelectDirection() {
         if (!manager.Mobile) {
-            direction.x = Input.GetAxisRaw("Horizontal");
-            direction.y = Input.GetAxisRaw("Vertical");
-            direction.Normalize();
+            moveDirection.x = Input.GetAxisRaw("Horizontal");
+            moveDirection.y = Input.GetAxisRaw("Vertical");
+            animDirection = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.localPosition;
         } else {
-            direction.x = gameController.MoveJoystick.Horizontal;
-            direction.y = gameController.MoveJoystick.Vertical;
+            moveDirection.x = gameController.MoveJoystick.Horizontal;
+            moveDirection.y = gameController.MoveJoystick.Vertical;
+
+            if (gameController.ShootJoystick.Direction != Vector2.zero)
+                animDirection = gameController.ShootJoystick.Direction;
         }
+
+        animDirection.Normalize();
+        moveDirection.Normalize();
     }
 
     private void SelectAnimation() {
-        if (direction == Vector2.zero) {
-            _animator.SetFloat(LASTDIRX, lastDirX);
-            _animator.SetFloat(LASTDIRY, lastDirY);
-            _animator.SetBool(ISMOVING, false);
-        } else {
-            lastDirX = direction.x;
-            lastDirY = direction.y;
-            _animator.SetFloat(DIRX, direction.x);
-            _animator.SetFloat(DIRY, direction.y);
-            _animator.SetBool(ISMOVING, true);
-        }
+        _animator.SetBool(IS_MOVING, moveDirection != Vector2.zero);
+        _animator.SetFloat(DIR_X, animDirection.x);
+        _animator.SetFloat(DIR_Y, animDirection.y);
+        isMovingUp = (animDirection.y > 0.866f); 
     }
 
     private void Die() {
         if (lives <= 0)
             _animator.SetBool(DIE, true);
     }
+
+
 }
